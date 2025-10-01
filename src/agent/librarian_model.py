50cc0929b3e0154280fa23e6b07d9f78b37c9569
imjuanleonard <julioanthonyleonard@gmail.com>
2025-10-01 21:56:03 +0200

    Limit number of output in list

--- src/agent/librarian_model.py
+++ src/agent/librarian_model.py
@@ -1,4 +1,4 @@
-from pydantic import BaseModel
+from pydantic import BaseModel, Field
 
 class CharacterAndRelationships(BaseModel):
     name: str
@@ -10,12 +10,12 @@ class ThemeSetting(BaseModel):
 
 class ContentInformation(BaseModel):
     genre: str
-    themes: list[str]
+    themes: list[str] = Field(min_length=3, max_length=10)
     setting: ThemeSetting
     cultural_context: str
     narrative_tone: str
     author_writing_style: str
-    characters_and_relationships: list[CharacterAndRelationships]
+    characters_and_relationships: list[CharacterAndRelationships] = Field(min_length=3, max_length=10)
 
 
 class BookMetadata(BaseModel):
@@ -24,9 +24,9 @@ class BookMetadata(BaseModel):
     publishing_year: int
     epub_id: str
     genre: str
-    themes: list[str]
+    themes: list[str] = Field(min_length=3, max_length=10)
     setting: ThemeSetting
     cultural_context: str
     narrative_tone: str
     author_writing_style: str
-    characters_and_relationships: list[CharacterAndRelationships]
+    characters_and_relationships: list[CharacterAndRelationships] = Field(min_length=3, max_length=10)
